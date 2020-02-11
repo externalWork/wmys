1359127f5dd2494c7a2859ed52f7751c0fdd2c74
wangyu <979222162@qq.com>
2020-02-11 12:16:47 +0800

    add keyords title description

--- src/App.js
+++ src/App.js
@@ -14,35 +14,58 @@ import { Product } from './Router/Product';
 import { Welfare } from './Router/Welfare';
 import { Brand } from './Router/Brand';
 import { About } from './Router/About';
+import { descriptions } from '../public/assets/json/descriptions.json'
+const routes = [
+  {
+    path: "/",
+    component: Home,
+    title: "雾美优视为护眼而生！",
+    content: descriptions[0]
+  },
+  {
+    path: ["/product/:name", "/product"],
+    component: Product,
+    title: "雾美优视套盒",
+    content: descriptions[1]
+  },
+  {
+    path: "/welfare",
+    component: Welfare,
+    title: "雾美优视关爱儿童视力健康，致力于给孩子一个明亮的未来。",
+    content: descriptions[2]
+  },
+  {
+    path: "/brand",
+    component: Brand,
+    title: "雾美优视品牌形象店，全国招商，期待您的加入。",
+    content: descriptions[3]
+  },
+  {
+    path: "/about",
+    component: About,
+    title: "雾美优视您身边的视力养护专家。",
+    content: descriptions[4]
+  }
+]
 
 
 export default function App() {
+  
   return (
     <Router className="App">
       <ScrollToTop>
         <Header />
         <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-
-          <Route path={["/product/:name", "/product"]}>
-            <Product />
-          </Route>
-
-          <Route path="/welfare">
-            <Welfare />
-          </Route>
-
-          <Route path="/brand">
-            <Brand />
-          </Route>
-
-          <Route path="/about">
-            <About />
-          </Route>
+          {
+            routes.map((route, i) => {
+              return  <Route exact={route.path === '/' ? true : false} key={i} path={route.path} render={props => {
+                document.title = route.title || "雾美优视";
+                document.querySelector('meta[name=description]').content = route.content;
+                return <route.component />
+              }} />
+            })
+          }
           <Redirect to="/" />
-
         </Switch>
         <Footer />
       </ScrollToTop>
